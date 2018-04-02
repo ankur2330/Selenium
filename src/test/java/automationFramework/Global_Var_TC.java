42edb38bf2c9bbedb2300f488d60d08a672e3522
ankur <ankurcy.in@gmail.com>
2018-04-01 18:08:06 -0600

    Selenium

--- src/test/java/automationFramework/Global_Var_TC.java
+++ src/test/java/automationFramework/Global_Var_TC.java
@@ -3,16 +3,16 @@ package automationFramework;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pageObjects.Home_Page;
-import pageObjects.Sign_in;
+import pageObjects.SignIn_Action;
 import utility.Constant;
 
 public class Global_Var_TC {
     public static WebDriver driver = null;
-    public static void  main(String args[]){
+    public static void  main(String args[]) throws Exception {
         System.setProperty("webdriver.gecko.driver","/home/ankur/Downloads/idea-IC-181.4203.550/geckodriver");
         driver = new FirefoxDriver();
         driver.get(Constant.URL);
-        Sign_in.Execute(driver,Constant.Username,Constant.Password);
+        SignIn_Action.Execute(driver);
         System.out.println("Login is Successful");
         driver.quit();
     }
